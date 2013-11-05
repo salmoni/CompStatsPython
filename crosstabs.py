f8f040d97ae61f811cd7354e3b1d37daaa5ed012
salmoni <salmoni@gmail.com>
2013-11-05 12:28:14 +0000

    Added cross-tabs
    Added cross-tabulated descriptive statistics routine

--- crosstabs.py
+++ crosstabs.py
@@ -1,21 +1,19 @@
-import numpy, scipy
+def GetGroups(groupingvars):
+    groups = []
+    k = len(groupingvars)
+    N = len(groupingvars[0])
+    for idx in range(N):
+        row = [var[idx] for var in groupingvars]
+        if row not in groups:
+            groups.append(row)
+    return groups
 
-"""
-A small collection of useful statistics routines
+def ExtractGroupsData(group, groupingvars, variable):
+    N = len(variable)
+    data = []
+    for idx in range(N):
+        row = [element[idx] for element in groupingvars]
+        if group == row:
+            data.append(variable[idx])
+    return data
 
-"""
-
-def GetIVs(x):
-    """
-    returns values within an independent variable
-    """
-    return numpy.array(list(set(x)))
-
-def GetAllCrossTabs(vars):
-    """
-    returns all crosstabbed values
-    """
-    k = len(vars)
-    # numpy.where((x == itemx) & (y == itemy))
-    # returns the indicaes of x[itemx] and y[itemy]
-    
